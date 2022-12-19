2aa22ce84b572c67b3dfe8435c4ca0c78b15b1a5
suhellynaraujo <github.interface@gmail.com>
2022-12-19 19:37:56 -0300

    número absoluto

---
+++ curso_programacao_java/src/_4funcoesMatematicas/exemplo_03_Math_abs.java
@@ -0,0 +1,25 @@
+package _4funcoesMatematicas;
+
+public class exemplo_03_Math_abs {
+
+	public static void main(String[] args) {
+		
+		// A varíavel recebe o valor absoluto, tirar o sinal negativo caso exista
+		//exemplo -1, entoa vai pegar o 1
+		// abs = absoluto
+			
+			double x = 3.0;
+			double y = 4.0;
+			double z = -5.0;
+			double A, B, C;
+			
+			A = Math.abs(x);
+			B = Math.abs(z);
+		
+			System.out.println("Valor absoluto de " + y +" = " + A);
+			System.out.println("Valor absoluto de " + y +" = " + B);
+		
+
+	}
+
+}
