cec5fd19df6b5521f4bb7118302dc389d9189847
suhellynaraujo <github.interface@gmail.com>
2022-12-19 19:25:02 -0300

    raiz quadrada

---
+++ curso_programacao_java/src/_4funcoesMatematicas/exemplo_01_Math_sqrt.java
@@ -0,0 +1,26 @@
+package _4funcoesMatematicas;
+
+public class exemplo_01_Math_sqrt {
+
+	public static void main(String[] args) {
+
+		// A varíavel recebe a raiz quadrada 
+		
+		double x = 3.0;
+		double y = 4.0;
+		double z = -5.0;
+		double A, B, C;
+		
+		A = Math.sqrt(x);
+		B = Math.sqrt(y);
+		C = Math.sqrt(25.0);
+		
+		System.out.println("Raiz quadrada de " + x + " = " + A);
+		System.out.println("Raiz quadrada de " + y + " = " + B);
+		System.out.println("Raiz quadrada de 25 = " + C);
+
+
+				
+	}
+
+}
