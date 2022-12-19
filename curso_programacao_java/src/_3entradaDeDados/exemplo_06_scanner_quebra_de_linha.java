26b73f23078468f5e167c455a7b449d8cd7f3ac1
suhellynaraujo <github.interface@gmail.com>
2022-12-19 18:57:32 -0300

    quebra de linha, nextLine

---
+++ curso_programacao_java/src/_3entradaDeDados/exemplo_06_scanner_quebra_de_linha.java
@@ -0,0 +1,29 @@
+package _3entradaDeDados;
+
+import java.util.Scanner;
+
+public class exemplo_06_scanner_quebra_de_linha {
+
+	public static void main(String[] args) {
+
+		// Entrada de dados = leitura de dados
+		// leitura de texto até a quebra de linha usando o nextLine()
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String s1, s2, s3;
+		
+		s1 = sc.nextLine();
+		s2 = sc.nextLine();
+		s3 = sc.nextLine();
+		
+		System.out.println("Dados digitados: ");
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+		
+		sc.close();
+
+	}
+
+}
