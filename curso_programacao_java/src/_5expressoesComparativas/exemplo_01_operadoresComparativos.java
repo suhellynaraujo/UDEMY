dfd0eb8cd48abf733ec8f5834bb2b6bd3e46ce8e
suhellynaraujo <github.interface@gmail.com>
2022-12-26 21:37:30 -0300

    Expressões comparativas

--- curso_programacao_java/src/_5expressoesComparativas/exemplo_01_operadoresComparativos.java
+++ curso_programacao_java/src/_5expressoesComparativas/exemplo_01_operadoresComparativos.java
@@ -3,7 +3,15 @@ package _5expressoesComparativas;
 public class exemplo_01_operadoresComparativos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// > maior
+		// < menor
+		// >= maior ou igual
+		// <= menor ou igual
+		// == igual
+		// != diferente
+		
+		
 
 	}
 
