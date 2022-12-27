467438ea7dd9f9651aac83c9b01e4193e2cb3b4f
suhellynaraujo <github.interface@gmail.com>
2022-12-27 20:13:42 -0300

    Expressões lógicas

---
+++ curso_programacao_java/src/_6expressoesLogicas/exemplo_01_operadoresLogicos.java
@@ -0,0 +1,18 @@
+package _6expressoesLogicas;
+
+public class exemplo_01_operadoresLogicos {
+
+	public static void main(String[] args) {
+		
+		// && e
+		// || ou
+		// ! não
+		
+		// Tabela Verdade
+		
+		// condição E = Todas as condições tem que ser VERDADEIRAS
+		// condição OU = Pelo menos uma condição deve ser VERDADEIRA
+		// condição NÃO =
+	}
+
+}
