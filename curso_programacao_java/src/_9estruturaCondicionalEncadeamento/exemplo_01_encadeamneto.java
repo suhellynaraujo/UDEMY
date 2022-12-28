ea061d053477d090ddff48922aa40ecd76105fb7
suhellynaraujo <github.interface@gmail.com>
2022-12-27 21:10:33 -0300

    Condição composta de encadeamento

---
+++ curso_programacao_java/src/_9estruturaCondicionalEncadeamento/exemplo_01_encadeamneto.java
@@ -0,0 +1,32 @@
+package _9estruturaCondicionalEncadeamento;
+
+import java.util.Scanner;
+
+public class exemplo_01_encadeamneto {
+
+	public static void main(String[] args) {
+
+		// Verdadeira executa somente o bloco do if
+		// Falso e executa somente o bloco do else
+
+		Scanner scan = new Scanner(System.in);
+		int hora;
+
+		System.out.println("Quantas horas? ");
+		hora = scan.nextInt();
+
+		if (hora < 12) {
+			System.out.println("Bom dia!");
+		}
+
+		else if (hora < 18) {
+			System.out.println("Boa tarde!");
+		} else {
+			System.out.println("Boa noite!");
+		}
+
+		scan.close();
+
+	}
+
+}
