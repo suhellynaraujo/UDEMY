19e4a62b0c686ad1b4b95c37d3f8632c357f4aa3
suhellynaraujo <github.interface@gmail.com>
2022-12-27 21:01:41 -0300

    Condição composta

---
+++ curso_programacao_java/src/_8estruturaCondicionalComposta/exemplo_01_if_else.java
@@ -0,0 +1,32 @@
+package _8estruturaCondicionalComposta;
+
+import java.util.Scanner;
+
+public class exemplo_01_if_else {
+
+	public static void main(String[] args) {
+		
+		// Verdadeira executa somente o bloco do if
+		// Falso e executa somente o bloco do else
+		
+		Scanner scan = new Scanner(System.in);
+		int hora;
+		
+		System.out.println("Quantas horas? ");
+		hora = scan.nextInt();
+		
+		if(hora < 12) {
+			System.out.println("Bom dia!");			
+		}
+		
+		else {
+			System.out.println("Boa tarde!");
+		}
+		
+		scan.close();
+		
+		
+
+	}
+
+}
