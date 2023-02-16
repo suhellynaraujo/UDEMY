0690ce613514d1e785c03f455024f5d802bca22a
suhellynaraujo <github.interface@gmail.com>
2023-02-16 20:47:51 -0300

    estrutura de repetição for, contagem regressiva

---
+++ curso_programacao_java/src/_13estruturaControleRepetitiva/exemplo_04_for3.java
@@ -0,0 +1,27 @@
+package _13estruturaControleRepetitiva;
+
+public class exemplo_04_for3 {
+	public static void main(String[] args) {
+		// for = estrutura de controle que repete um bloco de código (comandos),
+		// para um certo intervalo de valores. Usar quando se sabe a quantidade repetições ou o intervalo de valores
+		
+		/*Fazer um programa que lê um valor inteiro N e depois N numeros inteiros.
+		 * Ao final mostrar a soma dos N numeros lidos*/
+		
+		// for (inicio; condição; incremento ){}
+		
+		// for (executa somente na primeira vez; executa e volta; executa toda vez depois de voltar)
+		
+		// ideal para fazer uma rrepetição baseada em contagem
+		
+		
+		// contagem regressiva = decremento
+		for(int i = 4; i >= 0; i --) {
+			System.out.println("Valor de i: " + i);		
+			
+		}
+		
+	
+	}
+
+}
