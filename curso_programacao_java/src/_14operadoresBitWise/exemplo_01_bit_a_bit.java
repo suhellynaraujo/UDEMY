ca66096feb06eb484cc1719f4efeac0f6460c7f6
suhellynaraujo <github.interface@gmail.com>
2023-02-21 16:26:54 -0300

    bit a bit

---
+++ curso_programacao_java/src/_14operadoresBitWise/exemplo_01_bit_a_bit.java
@@ -0,0 +1,16 @@
+package _14operadoresBitWise;
+
+public class exemplo_01_bit_a_bit {
+
+	public static void main(String[] args) {
+		
+		//programação para baixo nível
+		int n1 = 89;
+		int n2 = 60;
+		System.out.println(n1 & n2);
+		System.out.println(n1 | n2);
+		System.out.println(n1 ^ n2);
+
+	}
+
+}
