0d615ff98d53897fe2424dfc74c0e81d212533d9
suhellynaraujo <github.interface@gmail.com>
2023-02-21 16:06:10 -0300

    estrutura de repetição do_while

---
+++ curso_programacao_java/src/_13estruturaControleRepetitiva/exemplo_05_do_while.java
@@ -0,0 +1,12 @@
+package _13estruturaControleRepetitiva;
+
+public class exemplo_05_do_while {
+	public static void main(String[] args) {
+		// O bloco de comandos executa pelo menos 1 vez, pois a condiçõa é verificada no final
+		// primeiro executa o bloco, depois verifica 
+		// passa pelo bloco, quando a condição for verdadeira,a estrutura volta e executa  novamente
+		// se a condição for falsa sai do bloco
+		
+	}
+
+}
