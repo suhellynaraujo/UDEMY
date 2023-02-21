89d61cd03024211712e96f6704524f5b86832e39
suhellynaraujo <github.interface@gmail.com>
2023-02-21 16:44:46 -0300

    trim

---
+++ curso_programacao_java/src/_15conversaoDeString/exemplo_03_trim.java
@@ -0,0 +1,16 @@
+package _15conversaoDeString;
+
+public class exemplo_03_trim {
+	public static void main(String[] args) {
+		// mantem a original removendo os espaços do final
+
+		String original = "abcd FGHIJ ABCD defg";
+
+		String convertida = original.trim();
+
+		System.out.println("Original: " + original);
+		System.out.println("toLowerCase: " + convertida);
+
+	}
+
+}
