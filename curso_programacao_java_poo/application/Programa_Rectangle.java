d3dde9031028cd9ae8e12abf99cb1fe4594d85d3
suhellynaraujo <github.interface@gmail.com>
2023-03-06 22:07:33 -0300

    retangulo

---
+++ curso_programacao_java_poo/application/Programa_Rectangle.java
@@ -0,0 +1,37 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Rectangle;
+
+public class Programa_Rectangle {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner(System.in);
+		
+		Rectangle retangulo =  new Rectangle();
+		
+		System.out.println("Enter rectangle width and height: ");
+		retangulo.width = sc.nextDouble();
+		retangulo.height = sc.nextDouble();
+		
+		
+		System.out.println(retangulo);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
