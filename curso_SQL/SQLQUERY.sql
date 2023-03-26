094d8f0f0c000371212db8873ef140164d7e95c3
suhellynaraujo <github.interface@gmail.com>
2023-03-26 18:51:25 -0300

    delimiter

--- curso_SQL/SQLQUERY.sql
+++ curso_SQL/SQLQUERY.sql
@@ -3611,3 +3611,81 @@ ORDER BY 1;
 | JOICE   | F    | JOICE@GMAIL.COM   | RES  | 77455786  | CENTRO     | RIO DE JANEIRO | RJ     |
 | LORENA  | F    | Não tem email     | COM  | 88687979  | COPACABANA | RIO DE JANEIRO | RJ     |
 +---------+------+-------------------+------+-----------+------------+----------------+--------+
+
+
+/*DELINITER*/
+
+STATUS
+
+--------------
+C:\Program Files (x86)\MySQL\MySQL Server 5.7\bin\mysql.exe  Ver 14.14 Distrib 5.7.20, for Win32 (AMD64)
+
+Connection id:          4
+Current database:
+Current user:           root@localhost
+SSL:                    Not in use
+Using delimiter:        ;
+Server version:         5.7.20-log MySQL Community Server (GPL)
+Protocol version:       10
+Connection:             localhost via TCP/IP
+Server characterset:    latin1
+Db     characterset:    latin1
+Client characterset:    cp850
+Conn.  characterset:    cp850
+TCP port:               3306
+Uptime:                 1 day 23 hours 30 min 40 sec
+
+Threads: 1  Questions: 155  Slow queries: 0  Opens: 113  Flush tables: 1  Open tables: 105  Queries per second avg: 0.000
+--------------
+
+/* Using delimiter:        ; */
+
+DELIMITER $
+
+STATUS
+
+--------------
+C:\Program Files (x86)\MySQL\MySQL Server 5.7\bin\mysql.exe  Ver 14.14 Distrib 5.7.20, for Win32 (AMD64)
+
+Connection id:          4
+Current database:
+Current user:           root@localhost
+SSL:                    Not in use
+Using delimiter:        $
+Server version:         5.7.20-log MySQL Community Server (GPL)
+Protocol version:       10
+Connection:             localhost via TCP/IP
+Server characterset:    latin1
+Db     characterset:    latin1
+Client characterset:    cp850
+Conn.  characterset:    cp850
+TCP port:               3306
+Uptime:                 1 day 23 hours 32 min 14 sec
+
+Threads: 1  Questions: 158  Slow queries: 0  Opens: 113  Flush tables: 1  Open tables: 105  Queries per second avg: 0.000
+--------------
+
+DELIMITER ;
+
+STATUS
+
+--------------
+C:\Program Files (x86)\MySQL\MySQL Server 5.7\bin\mysql.exe  Ver 14.14 Distrib 5.7.20, for Win32 (AMD64)
+
+Connection id:          4
+Current database:
+Current user:           root@localhost
+SSL:                    Not in use
+Using delimiter:        ;
+Server version:         5.7.20-log MySQL Community Server (GPL)
+Protocol version:       10
+Connection:             localhost via TCP/IP
+Server characterset:    latin1
+Db     characterset:    latin1
+Client characterset:    cp850
+Conn.  characterset:    cp850
+TCP port:               3306
+Uptime:                 1 day 23 hours 32 min 45 sec
+
+Threads: 1  Questions: 161  Slow queries: 0  Opens: 113  Flush tables: 1  Open tables: 105  Queries per second avg: 0.000
+--------------
