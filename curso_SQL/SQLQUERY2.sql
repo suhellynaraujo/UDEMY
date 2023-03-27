fc09453b601e6f22c8f9d8e3f1c58deea40d29b7
suhellynaraujo <github.interface@gmail.com>
2023-03-27 00:30:26 -0300

    QUERY2

---
+++ curso_SQL/SQLQUERY2.sql
@@ -0,0 +1,29 @@
+/* Oficina do Sr. José*/
+
+/*
+
+Cadastrar carros que entrarm para realizar serviços e seus donos.
+Cada cliente possui apenas um carro,
+Um carro possui apenas uma marca,
+As cores dos carros, 
+Os carros podem ter mais de uma cor,
+Armazenar os telefones dos  clientes, que não são obrigatórios.
+
+
+*/
+use projeto;
+
+show tables;
++-------------------+
+| Tables_in_projeto |
++-------------------+
+| cliente           |
+| livros            |
++-------------------+
+
+
+CREATE TABLE OFICINA(
+
+
+
+);
