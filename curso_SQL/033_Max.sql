dad4d0ad2ef3977fa0d2b66e5a1bc9985f822925
suhellynaraujo <github.interface@gmail.com>
2023-03-26 21:36:48 -0300

    Max

---
+++ curso_SQL/033_Max.sql
@@ -0,0 +1,10 @@
+/* MAX - TRAZ O VALOR MÁXIMO DE UMA COLUNA */
+
+SELECT MAX(VALOR) AS MAIOR_VALOR
+FROM CURSOS;
+
++-------------+
+| MAIOR_VALOR |
++-------------+
+|     6000.00 |
++-------------+
