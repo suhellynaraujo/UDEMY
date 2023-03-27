c9a0cdbd74080cddaa75f160370248ef2b39eda1
suhellynaraujo <github.interface@gmail.com>
2023-03-26 21:37:36 -0300

    Min

---
+++ curso_SQL/034_Min.sql
@@ -0,0 +1,10 @@
+/* MIN - TRAZ O VALOR MÍNIMO DE UMA COLUNA */
+
+SELECT MIN(VALOR) AS MENOR_VALOR
+FROM CURSOS;
+
++-------------+
+| MENOR_VALOR |
++-------------+
+|      500.00 |
++-------------+
