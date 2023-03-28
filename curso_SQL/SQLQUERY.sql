3bbe22f19cf11c702479b455ac38cf0752464c34
suhellynaraujo <github.interface@gmail.com>
2023-03-28 20:25:31 -0300

    TRIGGER

--- curso_SQL/SQLQUERY.sql
+++ curso_SQL/SQLQUERY.sql
@@ -4126,3 +4126,72 @@ SELECT NOME,
 | REACT                         |    35 | 3000.00 | 105000.00 |  85.714286 |
 | ANGULAR                       |    40 | 4500.00 | 180000.00 | 112.500000 |
 +-------------------------------+-------+---------+-----------+------------+
+
+
+
+
+
+/* 
+ESTRUTURA TRIGGER
+
+TRIGGER = GATILHO 
+
+FICA OLHANDO PARA TABELA E SÓ DISPARA ANTES OU DEPOIS DA INSTRUÇÃO
+
+SEMPRE LEMBRANDO DE MUDAR O DELIMITADOR ANTES E DEPOIS*/
+
+CREATE TRIGGER NOME
+BEFORE/AFTER INSERT/DELETE/UPDATE ON TABELA
+FOR EACH ROW (PARA CADA LINHA)
+BEGIN -> INICIO
+        QUALQUER COMANDO SQL
+END -> FIM
+
+CREATE TABLE USUARIO(
+    IDUSUARIO INT PRIMARY KEY AUTO_INCREMENT,
+    NOME VARCHAR(30) NOT NULL,
+    LOGIN VARCHAR(30) NOT NULL,
+    SENHA VARCHAR(30)
+);
+
+
+CREATE TABLE BKP_USUARIO(
+    IDBACKUP INT PRIMARY KEY AUTO_INCREMENT,
+    IDUSUARIO INT NOT NULL,
+    NOME VARCHAR(30) NOT NULL,
+    LOGIN VARCHAR(30) NOT NULL
+   
+);
+
+DELIMITER #
+
+CREATE TRIGGER BKP_USER
+BEFORE DELETE ON USUARIO
+FOR EACH ROW 
+
+BEGIN
+    
+    INSERT INTO BKP_USUARIO VALUES (NULL, OLD.IDUSUARIO, OLD.NOME, OLD.LOGIN);
+
+END 
+#
+
+DELIMITER ;
+
+INSERT INTO USUARIO VALUES(NULL, 'ANDRADE', 'ANDRADE2009', 'HEXACAMPEAO');
+
+SELECT * FROM USUARIO;
++-----------+---------+-------------+-------------+
+| IDUSUARIO | NOME    | LOGIN       | SENHA       |
++-----------+---------+-------------+-------------+
+|         1 | ANDRADE | ANDRADE2009 | HEXACAMPEAO |
++-----------+---------+-------------+-------------+
+
+DELETE FROM USUARIO WHERE IDUSUARIO = 1;
+
+SELECT * FROM BKP_USUARIO;
++----------+-----------+---------+-------------+
+| IDBACKUP | IDUSUARIO | NOME    | LOGIN       |
++----------+-----------+---------+-------------+
+|        1 |         1 | ANDRADE | ANDRADE2009 |
++----------+-----------+---------+-------------+
