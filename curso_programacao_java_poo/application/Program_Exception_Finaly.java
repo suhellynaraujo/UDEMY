dac8041d195813102fa6204ce00116a89463eba5
suhellynaraujo <github.interface@gmail.com>
2023-09-22 17:38:14 -0300

    finally

--- curso_programacao_java_poo/application/Program_Exception_Finaly.java
+++ curso_programacao_java_poo/application/Program_Exception_Finaly.java
@@ -7,16 +7,24 @@ import java.util.Scanner;
 public class Program_Exception_Finaly {
 	
 	public static void main(String[] args) {
+		//caminho do arquivo
 		File file = new File("C:\\temp\\in.txt");
 		Scanner sc = null;
 		try {
+			//busca o arquivo 
 			sc = new Scanner(file);
+			//percorre cada linha do arquivo 
 			while (sc.hasNextLine()) {
+				//imprime as linhas do arquivo
 				System.out.println(sc.nextLine());
 			}
-		} catch (IOException e) {
+		} 
+		catch (IOException e) {
 			System.out.println("Error opening file: " + e.getMessage());
-		} finally {
+			
+		} 
+		// executa independente se der exceção ou não
+		finally {
 			if (sc != null) {
 				sc.close();
 			}
